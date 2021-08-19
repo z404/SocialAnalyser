63643b0ddd1e147b454b420c6b025860fd6a50ad
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2021-08-19 19:21:55 +0530

    removed extention that doesnt work anymore

--- WhatsappDownloaderChrome/js/updateUI.js
+++
@@ -1,9 +0,0 @@
-(function(exp) {
-    
-    exp.set_success_fail_msg = (color, msg, display) => {
-        document.getElementById('status_div').style.display = display;
-        document.getElementById('success_fault').style.color = color;
-        document.getElementById('success_fault').innerHTML = msg;
-    }
-    
-})(this.updateUI = {});
