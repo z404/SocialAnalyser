3cd02f4d9dda052b3d6e8aa7f12e7ce5c73cad24
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2021-08-29 20:16:10 +0530

    trial for word cloud

---
+++ Whatsapp_word_cloud.py
@@ -0,0 +1,13 @@
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
+with open(input(), encoding="utf8") as file:
+    text = file.read()
+    text = text.replace("Media", "")
+    text = text.replace("ommitted", "")
+    text = text.replace("omitted", "")
+    text = text.replace("PM", "")
+wordcloud = WordCloud().generate(text)
+
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
